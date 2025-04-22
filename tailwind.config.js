09ba370dd77e410780bc8d8d33b04e0403ddba28
Hariel <hpadillas1700@alumno.ipn.mx>
2025-04-21 18:19:59 -0600

    init frontend

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    darkMode: "class",
+    theme: {
+      extend: {
+        colors: {
+          primary: "var(--color-primary)",
+          secondary: "var(--color-secondary)",
+          accent: "var(--color-accent)",
+          background: "var(--color-bg)",
+          text: "var(--color-text)",
+          "text-secondary": "var(--color-text-secondary)",
+          detail: "var(--color-detail)",
+        },
+      },
+    },
+    plugins: [],
+  }
+  
